b3e4a1698c1a0e6a16d6986f38edd9175db08e4d
unknown <begheli2020@gmail.com>
2024-01-23 18:15:20 -0300

    chore: vercel deploy config

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import routes from "./routes.js";
+import "dotenv/config";
+class App {
+  constructor() {
+    this.app = express();
+    this.app.use((req, res, next) => {
+      const allowedOrigins = [
+        'http://localhost:8080',
+        'http://localhost:3001',
+        'http://127.0.0.1:5500',
+        'https://devclub-audiotranscription.netlify.app/'
+      ];
+      const origin = req.headers.origin;
+      if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+      }
+      res.header(
+        'Access-Control-Allow-Methods',
+        'POST, GET, PUT, PATCH, OPTIONS, DELETE',
+      );
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.header('Acces-Control-Allow-Credentials', true);
+      next();
+    });
+    this.app.use(express.urlencoded({ extended: true, limit: '3mb' }));
+
+    this.middlewares();
+    this.routes();
+    // this.app.use(errorHandler);
+  }
+
+  middlewares() {
+    this.app.use(express.json({ limit: '3mb'}));
+  }
+
+  routes() {
+    this.app.use(routes);
+  }
+}
+
+const app = new App().app;
+
+app.listen(process.env.PORT || 3001, () => console.log(`Listening on port ${process.env.PORT}`));
---
+++ routes.js
@@ -0,0 +1,56 @@
+import { Router } from "express";
+import multer from "multer";
+import path from "path";
+import { speechToText } from "./speech-to-text.js";
+
+const routes = new Router();
+
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage, dest: "uploads/" });
+
+routes.get("/", async (req, res, next) => {
+  const healthCheck = {
+    uptime: process.uptime(),
+    responseTime: process.hrtime(),
+    message: "OK",
+    timestamp: Date.now(),
+  };
+
+  try {
+    res.send(healthCheck);
+  } catch (err) {
+    healthCheck.message = err;
+    return res.status(503).send();
+  }
+});
+
+routes.post("/get-mp3-file", upload.single("audio"), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("Nenhum arquivo foi enviado.");
+  }
+
+  try {
+    console.log("Arquivo recebido:", req.file.path);
+    const filePath = req.file.path;
+    const result = await speechToText(filePath);
+
+    res.status(200).json({ message: "Arquivo transcrevido", data: result });
+  } catch (error) {
+    console.error("Erro na transcrição:", error);
+    if (req.file && req.file.path) {
+      fs.unlinkSync(req.file.path);
+    }
+
+    res.status(500).json({
+      message: "Erro ao transcrever arquivo",
+      error: error.toString(),
+    });
+  }
+});
+
+export default routes;
