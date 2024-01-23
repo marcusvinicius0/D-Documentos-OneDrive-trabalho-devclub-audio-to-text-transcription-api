ee73d18efa1dd6a5fba1b143ca8fe625296574c8
unknown <begheli2020@gmail.com>
2024-01-23 18:25:50 -0300

    chore: vercel deploy config

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class App {
         'http://localhost:8080',
         'http://localhost:3001',
         'http://127.0.0.1:5500',
-        'https://devclub-audiotranscription.netlify.app/'
+        'https://devclub-audiotranscription.netlify.app',
       ];
       const origin = req.headers.origin;
       if (allowedOrigins.includes(origin)) {
