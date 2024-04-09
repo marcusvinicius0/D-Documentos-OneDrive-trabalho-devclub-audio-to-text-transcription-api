eb4e1abd9b472c4cbd8a59da926711981583db80
unknown <begheli2020@gmail.com>
2024-04-09 17:12:49 -0300

    fix: passing assistant and thread ids to chatbot

--- src/controllers/chatbase/chatbot/SaveNewAssistantController.js
+++ src/controllers/chatbase/chatbot/SaveNewAssistantController.js
@@ -3,10 +3,9 @@ import { runAssistant } from "../../../service/runAssistant.js";
 class SaveNewAssistantController {
   async store(req, res, next) {
     try {
-      const chatbotName = req.params.id;
       const { chatbot_id } = req.body;
 
-      const createRunAssistant = await runAssistant(chatbotName, chatbot_id);
+      const createRunAssistant = await runAssistant(chatbot_id);
 
       return res.status(200).json(createRunAssistant);
     } catch (error) {
